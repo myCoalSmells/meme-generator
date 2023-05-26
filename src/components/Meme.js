a35f193d0d464571649ddf5806c15c8f4b0d1b75
Michael Liu <michael.liu.mike@gmail.com>
2023-05-26 01:06:11 -0700

    images

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,12 +1,13 @@
-import React from "react"
+import React, {useState} from "react"
 import memesData from "../memesData"
 
 function Meme() {
+    const [memeUrl, setMemeUrl] = useState("")
+
     function getMemeImage() {
         const memesArray = memesData.data.memes;
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
-        console.log(url)
+        setMemeUrl(memesArray[randomNumber].url)
     }
 
     return(
@@ -16,7 +17,7 @@ function Meme() {
                 <input type="text" className="forms--input" placeholder="Bottom text"/>
                 <button className="forms--button" onClick={getMemeImage}>Get a new meme image 🖼</button>
             </div>
-            {/* <img src={require({memesData.data.memes[1].url})} /> */}
+            <img src={memeUrl} alt="meme-image" className="meme--image"/>
         </main>
 
     )
