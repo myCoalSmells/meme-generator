c4ac0e133d9391472c8712de6cff8cac52c7dc01
Michael Liu <michael.liu.mike@gmail.com>
2023-05-27 11:24:34 -0700

    add dynamic text to images

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -16,14 +16,28 @@ function Meme() {
         setMeme(prevMeme => ({...prevMeme, url: memesArray[randomNumber].url}))
     }
 
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(prev => {
+            return {
+                ...prev,
+                [name]: value
+            }
+        })
+    }
+
     return(
         <main className="meme">
             <div className="forms">
-                <input type="text" className="forms--input" placeholder="Top text"/>
-                <input type="text" className="forms--input" placeholder="Bottom text"/>
+                <input type="text" className="forms--input" placeholder="Top text" onChange={handleChange} name="topText" value={meme.topText}/>
+                <input type="text" className="forms--input" placeholder="Bottom text" onChange={handleChange} name="bottomText" value={meme.bottomText}/>
                 <button className="forms--button" onClick={getMemeImage}>Get a new meme image 🖼</button>
             </div>
-            <img src={meme.url} alt="meme-image" className="meme--image"/>
+            <div className="meme">
+                <img src={meme.url} alt="meme-image" className="meme--image"/>
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
         </main>
 
     )
